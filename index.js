f32f7dfcf2572ebb55f7223c0ed38cb8f1fd3d17
+ <erol.hendrickson@gmail.com>
2022-07-16 17:04:38 -0500

    notes for each code block

--- index.js
+++ index.js
@@ -6,23 +6,25 @@ const list = document.getElementById("monster-container");
 
 const sizes = document.getElementById('size-select');
 
-
+//Get whole array
 async function allMonsters() {
   const res = await fetch("https://www.dnd5eapi.co/api/monsters/")
   const data = await res.json()
   return data.results
 }
-
+//get each individiual array since each object of the main array has it's on URL to search fetch-ception
 async function getMonster(ref) {
   const res = await fetch("https://www.dnd5eapi.co" + ref.url)
   return res.json()
 }
 
+// async await outside of a function thanks to the module doc type
 const monster_refs = await allMonsters()
+// resolve all pomised data so we can sort it
 const monsters = await Promise.all(monster_refs.map(getMonster))
 
 
-
+// keeps an eye out for when the dropdown selection is made
 sizes.addEventListener("change", function() {
   if (sizes.value === "tiny"){
   let tinySelect =  monsters.filter(monster => monster.size === "Tiny")
@@ -92,13 +94,14 @@ sizes.addEventListener("change", function() {
   }}
   )
 
-
+//waits for rest button to be pressed to reload page
   const resetButton = document.getElementById("reset-button")
 
   let el = resetButton.addEventListener("click", function() {
     location.reload()
   })
 
+  // for flavor, changes text color on mouse hover, red can be anything but stuck wit the theme
   let hoverAction = resetButton.addEventListener("mouseover", function(e){
     e.target.style.color = "red";
     setTimeout(function() {
