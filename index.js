30cd97bb94384f269de3b3c3340ee0fc743e5edb
+ <erol.hendrickson@gmail.com>
2022-07-23 19:31:11 -0500

    .

--- index.js
+++ index.js
@@ -34,7 +34,6 @@ sizes.addEventListener("change", function() {
     <h2>${enemy.name}</h2>
     <p>${enemy.size}
     ${enemy.type}</p>
-  </div>
   `
   })
   list.innerHTML += tinyString.join(' ')
